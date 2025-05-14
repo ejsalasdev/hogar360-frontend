dac4cf25671dcc9fafa1bb07013560a82feaf198
Edgar J. Salas <ejsalasdev@gmail.com>
2025-05-14 09:52:34 -0500

    test: improve and add unit tests for location page (template-driven forms)

--- src/app/ui/components/pages/location-page/location-page.component.spec.ts
+++ src/app/ui/components/pages/location-page/location-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { DepartmentService } from '../../../../core/services/department.service';
+import { CityService } from '../../../../core/services/city.service';
 import { LocationPageComponent } from './location-page.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('LocationPageComponent', () => {
   let component: LocationPageComponent;
@@ -8,7 +12,10 @@ describe('LocationPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LocationPageComponent]
+      declarations: [LocationPageComponent],
+      imports: [HttpClientTestingModule, FormsModule],
+      providers: [DepartmentService, CityService],
+      schemas: [NO_ERRORS_SCHEMA]
     });
     fixture = TestBed.createComponent(LocationPageComponent);
     component = fixture.componentInstance;
@@ -17,5 +24,35 @@ describe('LocationPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have invalid form when fields are empty', () => {
+    expect(component.createLocationForm.valid).toBeFalsy();
+    expect(component.sector).toBe('');
+    expect(component.selectedDepartmentId).toBeNull();
+    expect(component.selectedCityId).toBeNull();
+  });
+
+  it('should validate sector minlength and maxlength', () => {
+    const sectorInput = component.createLocationForm.controls['sector'];
+    sectorInput.setValue('ab');
+    expect(sectorInput.errors?.['minlength']).toBeTruthy();
+    sectorInput.setValue('a'.repeat(51));
+    expect(sectorInput.errors?.['maxlength']).toBeTruthy();
+    sectorInput.setValue('Barrio Centro');
+    expect(sectorInput.errors).toBeNull();
+  });
+
+  it('should reset the form after creating a location', () => {
+    component.selectedDepartmentId = '1';
+    component.selectedCityId = '2';
+    component.sector = 'Sector Prueba';
+    fixture.detectChanges();
+    component.resetForm();
+    expect(component.selectedDepartmentId).toBeNull();
+    expect(component.selectedCityId).toBeNull();
+    expect(component.sector).toBe('');
+    expect(component.cities.length).toBe(0);
+    expect(component.createLocationForm.valid).toBeFalsy();
   });
 });
