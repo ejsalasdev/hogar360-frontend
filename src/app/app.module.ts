148b1ecb2c7139cc7cbcf1b101bf5b895c18f9db
Edgar J. Salas <ejsalasdev@gmail.com>
2025-05-18 16:52:03 -0500

    feat: add auth interceptor to send JWT in all requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { MoleculesModule } from './ui/components/molecules/molecules.module';
 import { PagesModule } from './ui/components/pages/pages.module';
 import { MainLayoutComponent } from './ui/layout/main-layout/main-layout.component';
 import { OrganismsModule } from './ui/components/organisms/organisms.module';
+import { AuthInterceptor } from './core/services/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent, MainLayoutComponent],
@@ -21,7 +23,13 @@ import { OrganismsModule } from './ui/components/organisms/organisms.module';
     PagesModule,
     OrganismsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
