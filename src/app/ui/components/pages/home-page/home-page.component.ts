43bf7b1c916f57b16525c86df6b5235e9b1bdbd2
Edgar J. Salas <ejsalasdev@gmail.com>
2025-05-18 21:30:57 -0500

    feat: add home-page base structure, Poppins font integration and footer update

---
+++ src/app/ui/components/pages/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent {
+  headerConfig = {
+    logoText: 'Hogar 360',
+    welcomeMessage: '',
+    userName: '',
+    userAvatarUrl: '',
+    showUserMenu: false
+  };
+}
